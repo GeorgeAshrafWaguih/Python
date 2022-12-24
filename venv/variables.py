1a9e59e9fbfdd7da28ce7fddc5c5f7b6c27c9ede
George <George.ashraf@hotmail.com>
2022-12-24 03:35:40 +0200

    Variables+Data

---
+++ venv/variables.py
@@ -0,0 +1,24 @@
+#Single line comment
+"""
+Multi line comment
+"""
+
+_cars = 23
+cars = 24
+CARS = 25
+number_of_cars = 26
+kind_of_car= "Ferrari"
+
+print(cars)
+print(CARS)
+print(number_of_cars)
+print(kind_of_car)
+
+name = "Janelle Jones"
+type_of_car = "Rolls Royce"
+school = "Foundation Elementry School"
+
+print(name + school)
+print(name + " works at "+ school)
+print(f"{name} works at {school}")
+
