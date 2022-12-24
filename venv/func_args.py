ed74f7fbeb22939ed38ed47520616525fdcd1f24
George <George.ashraf@hotmail.com>
2022-12-24 16:43:23 +0200

    Adding args and kwargs

---
+++ venv/func_args.py
@@ -0,0 +1,11 @@
+def user_info(name , age=0, city="Dubai", *args,**kwargs):
+    '''This function prints name , age and city
+    from an argument provided to the function '''
+
+    print(f"{name} is {age} years old from {city}")
+
+
+user_info("Geo", 34, "Cairo, Egypt")
+user_info("Janet")
+user_info(age=40, name="Trinity")
+user_info("Neo", 40, "Matrix", 7500, hire_date="September 2020")
